042c8de8ed50f660c8c0214b197d38c9f997c5c0
Sigfrid <camaldonadoc@unal.edu.co>
2021-04-22 07:49:22 -0500

    building print msg function

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
-console.log("ït works!")
+console.log("ït works!");
+
+// object structure
+
+const arrayPrincipal = [
+  {
+    nombre: "Canal1",
+    content: [
+      {
+        author: "Thomas Anderson",
+        time: "13:00",
+        avatar: "img/avatar.jpg",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      },
+      {
+        author: "Thomas Anderson",
+        time: "13:00",
+        avatar: "img/avatar.jpg",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      },
+      {
+        author: "Thomas Anderson",
+        time: "13:00",
+        avatar: "img/avatar.jpg",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      },
+    ],
+  },
+  {
+    nombre: "Canal2",
+    content: [
+      {
+        author: "Thomas Anderson",
+        time: "13:00",
+        avatar: "img/avatar.jpg",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      },
+      {
+        author: "Thomas Anderson",
+        time: "13:00",
+        avatar: "img/avatar.jpg",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      },
+      {
+        author: "Thomas Anderson",
+        time: "13:00",
+        avatar: "img/avatar.jpg",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      },
+    ],
+  },
+];
+console.log(arrayPrincipal);
+
+const $inputValue = document.querySelector("#messageBoxId");
+const $button = document.querySelector(".sendButton");
+
+
+const $messageBox=document.getElementsByClassName(".messageBox");
+
+const messageMetaData=document.createElement("div");
+        $messageBox.appendChild(messageMetaData)
+        
+
+        
+
+
+
+
+
+
+
+
